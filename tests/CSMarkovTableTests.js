52c5a614b35fe45dd29a4687d54d08467fc71a1b
Colin Sullivan <colinsul@gmail.com>
2012-12-06 21:10:44 -0800

    Continued refactor...
    PhraseGenerator now uses MultiStateMachines.
    Updated unit tests.

--- tests/CSMarkovTableTests.js
+++ tests/CSMarkovTableTests.js
@@ -20,17 +20,21 @@ var assert = require("assert"),
   CS = require("../lib/CS.js").CS,
   _ = require("../lib/vendor/underscore.js")._;
 require("../lib/CSMarkovTable.js");
+require("../lib/CSMarkovStateMachine.js");
 require("../lib/CSPhrase.js");
 require("../lib/CSMarkovPhraseGenerator");
 
-describe("CSMarkovTable", function () {
+describe("CS Markov Classes", function () {
   "use strict";
-  
-  describe("#next()", function () {
 
-    var table = new CS.MarkovTable({order: 2});
-    table.add_transition([60, 62, 64]);
-    table.add_transition([62, 64, 60]);
+  var table,
+    stateMachine;
+  
+  table = new CS.MarkovTable({order: 2});
+  table.add_transition([60, 62, 64]);
+  table.add_transition([62, 64, 60]);
+  
+  describe("CSMarkovTable", function () {
 
     it('should have the proper probability table', function () {
       // there should be only two transition rules
@@ -42,31 +46,36 @@ describe("CSMarkovTable", function () {
         "64": 1.0
       });
     });
+  });
 
-    
+  stateMachine = new CS.MarkovStateMachine({
+    table: table
+  });
+
+  describe("CSMarkovStateMachine", function () {
     it('should return 64 as the next state', function () {
       // stage previous state
-      table._prevStates = [60, 62];
-      assert.equal(table.next(), 64);
+      stateMachine._prevStates = [60, 62];
+      assert.equal(stateMachine.next(), 64);
 
       // no new transition rules should have been added
       assert.equal(_.keys(table._rows).length, 2);
     });
-
+    
     it('prev states should now be [62, 64]', function () {
-      assert.deepEqual(table._prevStates, [62, 64]);
+      assert.deepEqual(stateMachine._prevStates, [62, 64]);
       // no new transition rules should have been added
       assert.equal(_.keys(table._rows).length, 2);
     });
 
     it('should return 60 as next state', function () {
-      assert.equal(table.next(), 60);
+      assert.equal(stateMachine.next(), 60);
       // no new transition rules should have been added
       assert.equal(_.keys(table._rows).length, 2);
     });
 
     it('prev states should now be [64, 60]', function () {
-      assert.deepEqual(table._prevStates, [64, 60]);
+      assert.deepEqual(stateMachine._prevStates, [64, 60]);
       // no new transition rules should have been added
       assert.equal(_.keys(table._rows).length, 2);
     });
@@ -75,10 +84,20 @@ describe("CSMarkovTable", function () {
       var i;
 
       for (i = 0; i < 10; i++) {
-        assert.notEqual(typeof(table.next()), "undefined", new Error("table next was undefined when trying transition from [" + table._prevStates[0] + ", " + table._prevStates[1] + "]"));
+        assert.notEqual(typeof(stateMachine.next()),
+          "undefined",
+          new Error(
+            "state machine next was undefined when trying transition from [" +
+              stateMachine._prevStates[0] +
+              ", " +
+              stateMachine._prevStates[1] + "]"
+          )
+        );
       }
     });
+    
   });
+
 });
 
 describe("CSMarkovPhraseGenerator", function () {
